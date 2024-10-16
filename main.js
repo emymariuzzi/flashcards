0307731e755f4b4632258aca24bf75b6832b6a8b
emymariuzzi <161320976+emymariuzzi@users.noreply.github.com>
2024-10-16 12:11:56 -0300

    j

--- main.js
+++ main.js
@@ -18,8 +18,9 @@ let respostaestavisivel = false;
 function viraCartao(){
     respostaestavisivel = !respostaestavisivel;
     cartao.classList.toggle('active',respostaestavisivel);
+    cartao.addEventListener('click', viraCartao);
 }
-cartao.addEventListener('click', viraCartao);
+
 
 container.appendChild(cartao);
 }
